65bc43d765aec9e7c7478b23461b28fbdf3f5b3f
KDNT <783913139@qq.com>
2013-12-16 21:29:59 +0800

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -17,8 +17,8 @@ nets = dict(
     ###          with a relativly low difficulty, so maybe 60, which when we assume the pool 
     ###          gets every third block it's around 90 minutes, means a better distribution of
     ###          earnings, but newer miners will take some time to build up the income.
-    quarkcoin=math.Object(
-        PARENT=networks.nets['quarkcoin'],
+    Moleculecoin=math.Object(
+        PARENT=networks.nets['Moleculecoin'],
         SHARE_PERIOD=15, # seconds
         NEW_SHARE_PERIOD=15, # seconds
         CHAIN_LENGTH=24*60*60//10, # shares
@@ -28,17 +28,17 @@ nets = dict(
         NEW_SPREAD=30, # blocks
         IDENTIFIER='fc70135c7a81bc6f'.decode('hex'),
         PREFIX='9472ef181efcd37b'.decode('hex'),
-        P2P_PORT=8371,
+        P2P_PORT=11879,
         MIN_TARGET=0,
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=False,
-        WORKER_PORT=8372,
+        WORKER_PORT=18474,
         BOOTSTRAP_ADDRS=''.split(' '),
         #ANNOUNCE_CHANNEL='#p2pool',
         VERSION_CHECK=lambda v: True,
     ),
-    quarkcoin_testnet=math.Object(
-        PARENT=networks.nets['quarkcoin_testnet'],
+    Moleculecoin_testnet=math.Object(
+        PARENT=networks.nets['Moleculecoin_testnet'],
         SHARE_PERIOD=15, # seconds
         NEW_SHARE_PERIOD=15, # seconds
         CHAIN_LENGTH=24*60*60//10, # shares
@@ -48,7 +48,7 @@ nets = dict(
         NEW_SPREAD=30, # blocks
         IDENTIFIER='fc70235c7a81bc6f'.decode('hex'),
         PREFIX='8472ef181efcd37b'.decode('hex'),
-        P2P_PORT=18371,
+        P2P_PORT=,
         MIN_TARGET=0,
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=False,
