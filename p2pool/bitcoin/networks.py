c851a000ed5a183152bf62c07341b30b2e26635f
KDNT <783913139@qq.com>
2013-12-16 22:02:20 +0800

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -12,21 +12,21 @@ nets = dict(
     ###          Reward for the end of the regular mining period. Means: it will work now
     ###          and some time in the future. I think a simple max(..., 1) around it will fix it
     ###          Maybe the dust threshold should also be rised somewhat, since we only have 5 decimals...
-    quarkcoin=math.Object(
+    Moleculecoin=math.Object(
         P2P_PREFIX='fea503dd'.decode('hex'),
-        P2P_PORT=11973,
+        P2P_PORT=11879,
         ADDRESS_VERSION=58,
-        RPC_PORT=11972,
+        RPC_PORT=18474,
         RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'quarkcoinaddress' in (yield bitcoind.rpc_help()) and
+            'Moleculecoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         )),
         SUBSIDY_FUNC=lambda height: 2048*100000000 >> (height + 1)//60480,
-        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
-        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('Molecule_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('Molecule_hash').getPoWHash(data)),
         BLOCK_PERIOD=30, # s
         SYMBOL='QRK',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Quarkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Quarkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.quarkcoin'), 'quarkcoin.conf'),
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Moleculecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Moleculecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.Moleculecoin'), 'Moleculecoin.conf'),
         BLOCK_EXPLORER_URL_PREFIX='http://176.221.46.81/block/',
         ADDRESS_EXPLORER_URL_PREFIX='http://176.221.46.81/address/',
         ### Neisklar: normally 2**24 should be 2**20 BUT the quark enabled minerd is coded so that it only detects hashes below 0x000000xxxxxxx
@@ -38,21 +38,21 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.001e8,
     ),
-    quarkcoin_testnet=math.Object(
+    Molecule_testnet=math.Object(
         P2P_PREFIX='011a39f7'.decode('hex'),
-        P2P_PORT=18371,
+        P2P_PORT=11879,
         ADDRESS_VERSION=119,
-        RPC_PORT=18372,
+        RPC_PORT=18474,
         RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'quarkcoinaddress' in (yield bitcoind.rpc_help()) and
+            'Moleculecoinaddress' in (yield bitcoind.rpc_help()) and
             (yield bitcoind.rpc_getinfo())['testnet']
         )),
         SUBSIDY_FUNC=lambda height: 2048*100000000 >> (height + 1)//60480,
-        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
-        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('Molecule_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('Molecule_hash').getPoWHash(data)),
         BLOCK_PERIOD=30, # s
         SYMBOL='tQRK',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Quarkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Quarkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.quarkcoin'), 'quarkcoin.conf'),
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Moleculecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Moleculecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.Moleculecoin'), 'Moleculecoin.conf'),
         BLOCK_EXPLORER_URL_PREFIX='https://none.yet/block/',
         ADDRESS_EXPLORER_URL_PREFIX='https://none.yet/address/',
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**24 - 1),
